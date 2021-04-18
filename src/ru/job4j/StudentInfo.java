9a9142b41204586485ffa81052f33ffcd959b9c6
Sam <novosem@mail.ru>
2021-04-18 21:34:24 +0500

    3.3. Разбор первой программы. [#521118]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args){
+        System.out.println("Alexey Semin");
+        System.out.println("02.02.1972");
+    }
+}
