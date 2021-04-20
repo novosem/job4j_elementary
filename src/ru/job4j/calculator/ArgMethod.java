f43888b9b25c7487a3c218fbbf26ff910435941a
дизель-сервис <novosem@mail.ru>
2021-04-20 07:58:22 +0500

    4.1. Аргументы [#521109]

--- src/ru/job4j/calculator/ArgMethod.java
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -8,13 +8,8 @@ public class ArgMethod {
     public static void main(String[] args) {
         String name = "Petr Arsentev";
         int age = 33;
-
-        //ArgMethod.hello(name);
-
         ArgMethod.hello(name, age);
 
-        //ArgMethod.hello(age);
-
-        //ArgMethod.hello(name, name, name);
     }
+
 }
