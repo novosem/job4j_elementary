e8c3142e8346c5d127ee324c6f58ead540c87941
Sam <novosem@mail.ru>
2021-04-24 23:43:18 +0500

    5.7. Ипотека [#521158]

---
+++ src/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,13 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+
+    public static int year(int amount, int salary, double percent) {
+        int year = 1;
+        while (salary - amount * (100 + percent) / 100 < 0) {
+            year++;
+            amount = (int) (amount * (100 + percent) / 100) - salary;
+        }
+        return year;
+    }
+}
