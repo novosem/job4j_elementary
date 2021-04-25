4444c4558fb019fbc5474620b5ab77a3b38130b7
дизель-сервис <novosem@mail.ru>
2021-04-25 12:55:11 +0500

    5.5. Простое число [#521159] 1

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,17 +3,13 @@ package ru.job4j.loop;
 public class CheckPrimeNumber {
 
     public static boolean check(int number) {
-        boolean prime = true;
-        if (number <= 1) {
-            prime = false;
-        } else {
+        boolean prime = number > 1;
             for (int i = 2; i <= number / 2; i++) {
                     if (number % i == 0) {
                         prime = false;
                         break;
                     }
                 }
-            }
         return prime;
     }
 }
