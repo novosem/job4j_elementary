201e22ea915cb651bace93c61bcbd0324c032b3f
дизель-сервис <novosem@mail.ru>
2021-04-29 11:58:49 +0500

    6.8. Дефрагментация массива [#521093] 1

--- src/ru/job4j/array/Defragment.java
+++ src/ru/job4j/array/Defragment.java
@@ -6,11 +6,10 @@ public class Defragment {
         for (int index = 0; index < array.length; index++) {
             if (array[index] == null) {
                 int point = index;
-                for (int i = point; i < array.length; i++) {
+                for (int i = point + 1; i < array.length; i++) {
                     if (array[i] != null) {
-                        String temp = array[i];
-                        array[i] = array[index];
-                        array[index] = temp;
+                        array[index] = array[i];
+                        array[i] = null;
                         break;
                     }
                 }
