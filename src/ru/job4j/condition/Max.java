101c16b79eec0335a5406cf7e68871aba1d8eece
дизель-сервис <novosem@mail.ru>
2021-05-07 18:23:50 +0500

    3. Перегрузить метод max для трех чисел [#521265]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -6,8 +6,16 @@ public class Max {
         return left > right ? left : right;
     }
 
+    public static int max(int left, int right, int third) {
+        return max(left, max(right, third));
+    }
+
+    public static int max(int left, int right, int third, int fourth) {
+        return max(left, max(right, third, fourth));
+    }
+
     public static void main(String[] args) {
-        int rsl = Max.max(1, 2);
+        int rsl = Max.max(1, 2, 3, 4);
         System.out.println("Максимальное число " + rsl);
     }
 }
--- test/ru/job4j/condition/MaxTest.java
+++ test/ru/job4j/condition/MaxTest.java
@@ -31,4 +31,35 @@ public class MaxTest {
         int result = Max.max(left, right);
         Assert.assertEquals(expected, result);
     }
+
+    @Test
+    public void max3() {
+        int left = 2;
+        int right = 3;
+        int third = 4;
+        int expected = 4;
+        int result = Max.max(left, right, third);
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void max4() {
+        int left = 1;
+        int right = 2;
+        int third = 3;
+        int fourth = 4;
+        int expected = 4;
+        int result = Max.max(left, right, third, fourth);
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void max5() {
+        int left = 2;
+        int right = 2;
+        int third = 2;
+        int expected = 2;
+        int result = Max.max(left, right, third);
+        Assert.assertEquals(expected, result);
+    }
 }
